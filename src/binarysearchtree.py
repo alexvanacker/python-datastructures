2cb32f10f7eb2a5e9cec1ad18040ff8ca2a105f7
Alex Vanacker <alexvanacker@gmail.com>
2017-07-08 15:13:01 +0200

    Fix delete method, and add_value method
    When adding a value to the tree, base the comparisons on the key and
    not the value.
    Delete now properly works.
    Added print method for debugging (far from perfect) based on BSF.
    Add UT for a more complex tree build, and for delete.

--- src/binarysearchtree.py
+++ src/binarysearchtree.py
@@ -27,9 +27,9 @@ class BinarySearchTree():
             node = Node(key, value)
             return node
 
-        if node.value > value:
+        if node.key > key:
             node.left = self.add_value_to_node(key, value, node.left)
-        elif node.value < value:
+        elif node.key < key:
             node.right = self.add_value_to_node(key, value, node.right)
         else:
             # Overwrite the previous value
@@ -56,7 +56,66 @@ class BinarySearchTree():
             return self.get_from_node(key, node.right)
 
     def delete(self, key):
-        pass
+        self.rootNode = self.delete_from_node(key, self.rootNode)
+
+    def delete_from_node(self, key, node):
+        if key < node.key:
+            node.left = self.delete_from_node(key, node.left)
+            return node
+        elif key > node.key:
+            node.right = self.delete_from_node(key, node.right)
+            return node
+        else:
+            if node.left is None:
+                return node.right
+
+            if node.right is None:
+                return node.left
+            else:
+                # We find the minimum node in the right subtree
+                # we will replace the deleted node by that node
+                copy_node = node
+                node = copy_node.right.min()
+                print 'Min in right tree found: {}'.format(node.key)
+                # delete that one from the right tree
+                node.right = self.delete_from_node(node.key, copy_node.right)
+                node.left = copy_node.left
+                return node
+
+    def print_tree(self):
+        """ Debug function, very raw, based on BSF algorithm."""
+
+        visited = {}
+        # List of list of node keys belonging to the same layer
+        layers = []
+        layers.append([self.rootNode])
+        layer_index = 0
+        while len(layers[layer_index]) > 0:
+            #  Init next layer
+            next_layer = layer_index + 1
+            layers.insert(next_layer, [])
+            for node in layers[layer_index]:
+                if node.key not in visited:
+                    # Process that node
+                    visited[node.key] = True
+                    if node.left is not None:
+                        print 'adding {} to layer {}'.format(node.left.key,
+                                                             next_layer)
+                        layers[layer_index + 1].append(node.left)
+
+                    if node.right is not None:
+                        print 'adding {} to layer {}'.format(node.right.key,
+                                                             next_layer)
+                        layers[layer_index + 1].append(node.right)
+
+            # Increment layer index
+            layer_index += 1
+
+        for layer in layers:
+            layer_string = ''
+            for node in layer:
+                layer_string += str(node.key) + '  '
+            print layer_string
 
 
 class Node():
@@ -68,6 +127,14 @@ class Node():
         self.value = value
         self.left = None
         self.right = None
+
+    def print_node(self, index=0):
+        tab = ''
+        for x in xrange(index):
+            tab += ' '
+        print tab + str(self.key)
+        print tab + '[]' if self.left is None else self.left.print_node(index=index+1)
+        print tab + '[]' if self.right is None else self.right.print_node(index=index+1)
 
     def size(self):
         if self is None:
@@ -86,4 +153,3 @@ class Node():
     def __eq__(self, other):
         return self.key == other.key and \
             self.value == other.value
-
--- src/test_binarysearchtree.py
+++ src/test_binarysearchtree.py
@@ -20,6 +20,20 @@ class Test(unittest.TestCase):
         node1 = tree.get(1)
         self.assertEquals(1, node1.size())
 
+    def test_construction(self):
+        tree = bst.BinarySearchTree()
+        tree.add_value(2, 'b')
+        tree.add_value(1, "a")
+        tree.add_value(4, 'c')
+        tree.add_value(3, 'd')
+        tree.add_value(7, 'e')
+        tree.add_value(5, 'f')
+        tree.add_value(8, 'g')
+
+        node4 = tree.get(4)
+        self.assertEquals(7, node4.right.key)
+        self.assertEquals(3, node4.left.key)
+
     def test_get_dont_exist(self):
         tree = bst.BinarySearchTree()
         tree.add_value(2, 'b')
@@ -35,6 +49,31 @@ class Test(unittest.TestCase):
 
         node = tree.min()
         self.assertEquals(1, node.key)
+
+    def test_delete(self):
+        tree = bst.BinarySearchTree()
+        tree.add_value(2, 'b')
+        tree.add_value(1, "a")
+        tree.add_value(4, 'c')
+        tree.add_value(3, 'd')
+        tree.add_value(7, 'e')
+        tree.add_value(5, 'f')
+        tree.add_value(8, 'g')
+        tree.print_tree()
+        self.assertEquals(2, tree.rootNode.key)
+        tree.delete(4)
+
+        tree.print_tree()
+
+        # Should be what replaced the node with key 4
+        node5 = tree.get(5)
+        self.assertEquals(3, node5.left.key)
+        self.assertEquals(7, node5.right.key)
+
+
+
+
+
 
 
 if __name__ == '__main__':
